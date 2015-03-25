82ef85c8766e9332a57af8b6dc63c2944829ee20
torvik <pless.victor@gmail.com>
2015-03-24 22:16:49 -0300

    remove show and index reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :reviews
+  resources :reviews, except: [:show, :index]
 
   devise_for :users
   resources :restaurants
 
   get 'pages/about'
-
   get 'pages/contact'
 
   root 'restaurants#index'
