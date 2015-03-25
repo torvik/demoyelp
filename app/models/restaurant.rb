81d3d2295c66b85e13ff10c759c1e560099834db
torvik <pless.victor@gmail.com>
2015-03-24 22:30:02 -0300

    Link reviews with restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,6 @@ class Restaurant < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
+  has_many :reviews
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  resources :reviews, except: [:show, :index]
+
+   root 'restaurants#index'
 
   devise_for :users
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, except: [:show, :index]
+  end
 
   get 'pages/about'
   get 'pages/contact'
 
-  root 'restaurants#index'
+
 
 end
