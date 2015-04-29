e5e9d2402d3f6839ca4272a84478b67b076ef26f
torvik <pless.victor@gmail.com>
2015-04-29 13:50:07 -0300

    Add date validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,17 @@ class Restaurant < ActiveRecord::Base
 
   has_many :reviews
 
+  validates :name, :address, :phone, :website, :image, presence: true
+
+   validates :phone, format: { with: /\A\(\d{3}\) \d{5}-\d{4}\z/,
+    message: "format (000) 00000-0000" }
+
+    validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "format https://www.example.com" }
+
+    validates :address, format: { with: /\A[^,]+, \d+ [a-zA-Z]+-[A-Z]{2}\z/,
+    message: "format : Nome da Rua, Número Cidade- Sigla Estado exemplo: Rua Teste, 128 Rio de Janeiro-RJ" }
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 
   has_many :reviews, dependent: :destroy
 
+  validates :first_name, :last_name, presence: true
+
 end
